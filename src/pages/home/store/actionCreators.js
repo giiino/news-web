1ed348341f1b63437148481679774d80986b3077
giiino <52125591+giiino@users.noreply.github.com>
2022-03-11 13:34:15 +0800

    Update actionCreators.js

--- src/pages/home/store/actionCreators.js
+++ src/pages/home/store/actionCreators.js
@@ -71,34 +71,3 @@ export const getNewsData = () =>{
 
     }
 }
-
-
-// export const getNewsData = () =>{
-//     return (dispatch) =>{
-//         let Today = new Date()
-//         const fromDate = Today.getFullYear()+'-'+(Today.getMonth()+1).toString().padStart(2, '0')+'-'+(Today.getDate()-3).toString().padStart(2, '0')
-//         const toDate = Today.getFullYear()+'-'+(Today.getMonth()+1).toString().padStart(2, '0')+'-'+(Today.getDate()).toString().padStart(2, '0')
-//         axios.all([
-//             axios.get(`https://newsapi.org/v2/everything?q=covid-19&from=${fromDate}&to=${toDate}&apiKey=${process.env.REACT_APP_ARTICLE_API_KEY}`),
-//             axios.get(`https://newsapi.org/v2/everything?q=taiwan&from=${fromDate}&to=${toDate}&apiKey=${process.env.REACT_APP_ARTICLE_API_KEY}`),
-//             axios.get(`https://newsapi.org/v2/everything?q=china&from=${fromDate}&to=${toDate}&apiKey=${process.env.REACT_APP_ARTICLE_API_KEY}`),
-//             axios.get(`https://newsapi.org/v2/everything?q=usa&from=${fromDate}&to=${toDate}&apiKey=${process.env.REACT_APP_ARTICLE_API_KEY}`),
-//             axios.get(`https://newsapi.org/v2/everything?q=anime&from=${fromDate}&to=${toDate}&apiKey=${process.env.REACT_APP_ARTICLE_API_KEY}`)
-//         ]).then(axios.spread(function (resA, resB, resC, resD, resE){
-//             dispatch(newsDataAction(modifyAjaxData(resA.data.articles), modifyAjaxData(resB.data.articles), modifyAjaxData(resC.data.articles), modifyAjaxData(resD.data.articles), modifyAjaxData(resE.data.articles)))
-//             // let testlist = resA.data.articles.filter((item)=>{
-//             //     return item.author === 'Brenda Goodman, CNN'
-//             // })
-//             // console.log(testlist)
-//             // let testlist = resB.data.articles.map(item=>{
-//             //     return {
-//             //         ...item, 
-//             //         viewer:0, 
-//             //         like:0, 
-//             //         comment:[]
-//             //     }
-//             // })
-//             // console.log(...testlist)
-//         }))
-//     }
-// }
